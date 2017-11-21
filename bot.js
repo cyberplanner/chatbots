2af42744e190c18d5acd28f4a090c6d921dabbc7
cyberplanner <saleh.gfx@gmail.com>
2017-11-21 14:19:02 +0000

    Use ChatConnector instead of ConsoleConnector to connect to bot emulator

--- bot.js
+++ bot.js
@@ -1,5 +1,21 @@
+const restify = require('restify');
 const builder = require('botbuilder');
-const connector = new builder.ConsoleConnector().listen();
+
+// Create server
+const server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, ()=> {
+  console.log('Listening to', server.name, server.url);
+});
+
+// Create chat connector for communicating with the Bot Framework Service
+const connector = new builder.ChatConnector({
+  appId: process.env.MS_APP_ID,
+  appPassword: process.env.MS_APP_PASSWORD
+});
+
+// Listen for messages from users
+server.post('/api/messages/', connector.listen());
+
 const bot = new builder.UniversalBot(connector, (session) => {
   session.send("You said: " + session.message.text);
 });
